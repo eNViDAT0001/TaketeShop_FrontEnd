c03d12d393aaccb92f13de2b0fe88715a29bfea7
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2022-04-17 08:31:16 +0700

    chat

--- src/navigation/AccountNavigator.js
+++ src/navigation/AccountNavigator.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { ACCOUNT_MAIN_SCREEN } from '../constants/NavigatorIndex';
+import { ACCOUNT_MAIN_SCREEN,CHAT_SCREEN } from '../constants/NavigatorIndex';
 import AccountMainScreen from '../screens/accountScreen/AccountMainScreen';
 import ProfileNavigation from './ProfileNavigation';
 import ChatScreen from '../screens/accountScreen/ChatScreen/ChatScreen';
@@ -15,7 +15,7 @@ function AccountNavigator() {
         options={{ headerShown: false }}
         component={AccountMainScreen}></Stack.Screen>
       <Stack.Screen
-        name={'ChatScreen'}
+        name={CHAT_SCREEN}
         options={{ headerShown: false }}
         component={ChatScreen}></Stack.Screen>
     </Stack.Navigator>
--- src/screens/accountScreen/ChatScreen/ChatScreen.js
+++ src/screens/accountScreen/ChatScreen/ChatScreen.js
@@ -1,6 +1,6 @@
 import React, { useState, useRoute } from 'react';
-import { Text, StyleSheet, View, Image, TouchableOpacity, ScrollView } from 'react-native'
-import { TextInput, Colors, IconButton, Button } from 'react-native-paper';
+import { Text, StyleSheet, View, Image, TouchableOpacity, ScrollView, TextInput } from 'react-native'
+import { Colors, IconButton, Button } from 'react-native-paper';
 import { Dropdown } from 'react-native-element-dropdown';
 import { useNavigation } from '@react-navigation/native';
 import Header from '../../../components/UI/Header';
@@ -8,22 +8,31 @@ import Header from '../../../components/UI/Header';
 
 function ChatScreen(props) {
   const [value, setValue] = useState(null);
+  const [messages, setMessages] = useState(null);
   const navigation = useNavigation()
   return (
     <View style={styles.screen}>
       <Header title="Hỗ trợ khách hàng"></Header>
-      <ScrollView style = {styles.chatcontainer}>
+      <ScrollView style={styles.chatcontainer}>
         <Text>asdsa</Text>
       </ScrollView>
 
-      <View style={styles.typeChatcontainer}>
+      <View style={styles.sendChatcontainer}>
         <TextInput
-          multiline={true}
-          placeholder='type your message'
           style={{
             height: 45,
-
+            width: 330,
+            backgroundColor: '#e7e7e7',
           }}
+          onChangeText={setMessages}
+          value={messages}
+          placeholder='type your message'         
+        />
+        <IconButton
+          icon="send"
+          color='#2196f3'
+          size={25}
+          onPress={() => console.log('Pressed')}
         />
       </View>
 
@@ -42,15 +51,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffff',
   },
 
-  chatcontainer:{
-   
+  chatcontainer: {
+
   },
 
 
-  typeChatcontainer: {
+  sendChatcontainer: {
     padding: 10,
-    borderTopColor: '#d4d4d4',   
+    borderTopColor: '#d4d4d4',
     flexDirection: 'row',
+    width: 300,
   },
 
 });
