475c6d1b2bb2bad2d0b9be52cf79dd51c1455f43
yyyanhkhoa <80150282+yyyanhkhoa@users.noreply.github.com>
2022-05-14 09:16:33 +0700

    up

--- src/screens/accountScreen/ListStaffScreen.js
+++ src/screens/accountScreen/ListStaffScreen.js
@@ -6,53 +6,124 @@ import {
   SafeAreaView,
   Image,
   TouchableOpacity,
-  ScrollView,
+  FlatList,
 } from 'react-native';
 import Form1 from '../../accountScreen/Form';
-import { TextInput, Button, Colors, IconButton } from 'react-native-paper';
+import { TextInput, Button, Colors, IconButton, Avatar } from 'react-native-paper';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
-import { Avatar } from 'react-native-paper';
 import Header from '../../../components/UI/Header';
 import { useSelector, useDispatch } from 'react-redux';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import * as authActions from '../../../store/actions/auth'
-
+import * as chanelActions from '../../../store/actions/chanelActions';
+import { Form } from 'formik';
 
 function ListStaffScreen() {
+  var ALL_LIST_CHANEL = useSelector(state => state.chanel.LIST_CHANEL);
   const navigation = useNavigation();
   const dispatch = useDispatch();
-  const name = (useSelector(state => state.auth.name));
- 
+  let chanelID = useSelector(state => state.chanel._id);
+  let img = useSelector(state => state.auth.avatar);
+
+  useEffect(() => {
+    dispatch(chanelActions.getMessagerFromChanelId(chanelID));
+
+  });
+  const Chats = (item) => {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => {
+            console.log("goi chanel userID:" + item.avatar)
+            dispatch(chanelActions.getChanel(item.userID));
+            dispatch(chanelActions.getMessagerFromChanelId(chanelID)).then(
+              navigation.navigate('ChatScreen' ,{titleHeader: item.name})
+            )
+            
+
+          }}>
+          <View style={styles.screenrow}>
+            <Avatar.Image
+              size={50}
+              source={{ uri: item.avatar }}
+            />
+            <View>
+              <Text style={styles.text1}>
+                {item.name}</Text>
+              <View style={styles.text2}>
+                <Text>  {item.type}</Text>
+
+              </View>
+            </View>
+            <View style={{
+              flex: 1,
+              flexDirection: 'row',
+              alignItems: 'center',
+              justifyContent: 'flex-end',
+            }}>
+              <MaterialCommunityIcons
+                name="chevron-right"
+                color={Colors.iconColor}
+                size={40}
+              />
+            </View>
+
+          </View>
+        </TouchableOpacity>
+      </View >
+
+    )
+  };
+  const renderMessages = (item) => {
+    return (
+      Chats(item)
+    )
+  };
 
   return (
-    <ScrollView style={styles.container}>
-      <Header title="Thông tin nhân viên"></Header>
-    
-      <Form1
-        icons="account"
-        titletext="Tên tài khoản"
-        onPress={() => {
-         // navigation.navigate('ChangeName', { value: name });
-        }}
-        titletext2={name}
+
+    <SafeAreaView style={styles.screen}>
+      <Header title="Danh sách hỗ trợ khách hàng"></Header>
+
+      <FlatList
+        data={ALL_LIST_CHANEL}
+        extraData={ALL_LIST_CHANEL}
+        renderItem={itemData => (renderMessages(itemData.item))}
+        keyExtractor={(item, index) => item._id}
+        contentContainerStyle={{ flexGrow: 1, backgroundColor: '#D3D3D388', top: 5, marginHorizontal: 8 }}
+
       />
-     
-    </ScrollView>
+
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    backgroundColor: '#d4d4d4',
+  },
   container: {
+    borderRadius: 20,
+    margin: 10,
     flex: 1,
     backgroundColor: '#ffff',
   },
-  containeravatar: {
-    backgroundColor: '#ffff',
-    alignContent: 'center',
+  screenrow: {
     flexDirection: 'row',
-    padding: 10,
+    alignItems: 'center',
+    padding: 5,
+    flex: 1,
+  },
+  expandContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-end',
   },
   text1: {
-    fontSize: 30,
+    left: 7,
+    fontSize: 20,
     fontWeight: '900',
     fontFamily: 'open-sans-bold',
     textShadowRadius: 1,
