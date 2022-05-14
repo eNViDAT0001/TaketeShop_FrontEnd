66cd36403ac148d0680eddbf944828eb2f39efad
eNViDAT-student <nvdat.ngvan@gmail.com>
2022-05-14 23:26:17 +0700

    fix UI

--- src/navigation/AccountNavigator.js
+++ src/navigation/AccountNavigator.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import { FlatList, SafeAreaView, StatusBar, StyleSheet, Text, TouchableOpacity } from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { ACCOUNT_MAIN_SCREEN, CHAT_SCREEN,LIST_CHANEL , LIST_STAFF_SCREEN } from '../constants/NavigatorIndex';
 import AccountMainScreen from '../screens/accountScreen/AccountMainScreen';
-import ProfileNavigation from './ProfileNavigator';
 import ChatScreen from '../screens/accountScreen/ChatScreen/ChatScreen';
 import ListChanel from '../screens/accountScreen/ChatScreen/ListChanel';
 import ListStaffScreen from '../screens/accountScreen/ADMIN/ListStaffScreen'
